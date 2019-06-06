01b94cadc709f66ca6e4edd10e5ad0dd33c53479
David Bratton <debratton@comcast.net>
2019-06-06 17:06:36 -0500

    06062019

---
+++ RunTracker/RunTracker/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  RunTracker
+//
+//  Created by David E Bratton on 6/6/19.
+//  Copyright © 2019 David Bratton. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RunTracker/RunTracker/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  RunTracker
+//
+//  Created by David E Bratton on 6/6/19.
+//  Copyright © 2019 David Bratton. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
