265d66c90f92fe9377a6d3d4fbcee7f073d876d3
David Bratton <debratton@comcast.net>
2019-06-08 17:37:07 -0500

    Done for day

--- RunTracker/RunTracker/Controller/CurrentRunVC.swift
+++ RunTracker/RunTracker/Controller/CurrentRunVC.swift
@@ -7,22 +7,67 @@
 //
 
 import UIKit
+import MapKit
 
 class CurrentRunVC: LocationVC {
 
     @IBOutlet weak var swipeBGImageView: UIImageView!
     @IBOutlet weak var sliderImageView: UIImageView!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var paceLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var pauseBtn: UIButton!
     
+    // 3 variables needed for keeping track of run
+    var startLocation: CLLocation!
+    var lastLocation: CLLocation!
+    var timer = Timer()
+    var runDistance = 0.0
+    var pace = 0
+    var counter = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         let swipeGesture = UIPanGestureRecognizer(target: self, action: #selector(endRunSwiped(sender:)))
         sliderImageView.addGestureRecognizer(swipeGesture)
         sliderImageView.isUserInteractionEnabled = true
         swipeGesture.delegate = self as? UIGestureRecognizerDelegate
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        manager?.delegate = self
+        manager?.distanceFilter = 10
+        startRun()
+    }
+    
+    func startRun() {
+        manager?.startUpdatingLocation()
+        startTimer()
+    }
+    
+    func endRun() {
+        manager?.stopUpdatingLocation()
+    }
+    
+    func startTimer() {
+        durationLabel.text = counter.formatTimeDurationToString()
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateCounter() {
+        counter += 1
+        durationLabel.text = counter.formatTimeDurationToString()
+    }
+    
+    func calculatePace(time seconds: Int, miles: Double) -> String {
+        pace = Int(Double(seconds) / miles)
+        return pace.formatTimeDurationToString()
+    }
+    
+    @IBAction func pauseBtnPressed(_ sender: UIButton) {
         
     }
+    
     
     @objc func endRunSwiped(sender: UIPanGestureRecognizer) {
         let minAdjust: CGFloat = 80
@@ -46,5 +91,26 @@ class CurrentRunVC: LocationVC {
                 }
             }
         }
+    }
+}
+
+extension CurrentRunVC: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            checkLocationAuthStatus()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if startLocation == nil {
+            startLocation = locations.first
+        } else if let location = locations.last {
+            runDistance += lastLocation.distance(from: location)
+            distanceLabel.text = "\(runDistance.metersToMiles(places: 2))"
+            if counter > 0 && runDistance > 0 {
+                paceLabel.text = calculatePace(time: counter, miles: runDistance.metersToMiles(places: 2))
+            }
+        }
+        lastLocation = locations.last
     }
 }
---
+++ RunTracker/RunTracker/Extensions/Extensions.swift
@@ -0,0 +1,33 @@
+//
+//  Extensions.swift
+//  RunTracker
+//
+//  Created by David E Bratton on 6/8/19.
+//  Copyright © 2019 David Bratton. All rights reserved.
+//
+
+import Foundation
+
+extension Double {
+    func metersToMiles(places: Int) -> Double {
+        let divisor = pow(10.0, Double(places))
+        return ((self / 1609.34) * divisor).rounded() / divisor
+    }
+}
+
+extension Int {
+    func formatTimeDurationToString() -> String {
+        let durationHours = self / 3600
+        let durationMinutes = (self % 3600) / 60
+        let durationSeconds = (self % 3600) % 60
+        if durationSeconds < 0 {
+            return "00:00:00"
+        } else {
+            if durationHours == 0 {
+                return String(format: "%02d:%02d", durationMinutes, durationSeconds)
+            } else {
+                return String(format: "%02d:%02d:%02d", durationHours, durationMinutes, durationSeconds)
+            }
+        }
+    }
+}
